c9da20c5ad9262aab402bcce769ac37a6f8db4e2
weiconglyu <weiconglyu@outlook.com>
2025-11-14 22:06:08 +0800

    Author updated

--- R/shiny.R
+++ R/shiny.R
@@ -9,6 +9,8 @@ shinyVEMIRT <- function() {
 #' Shiny App for DIF Dashboard
 #'
 #' @author Yijun Cheng <chengxb@uw.edu>
+#' @author He Ren <heren@uw.edu>
+#' @author Weicong Lyu <weiconglyu@um.edu.mo>
 #' @export
 DIFdashboard <- function() {
   suppressWarnings(shiny::runApp(system.file('DIFdashboard', package = 'VEMIRT'), display.mode = 'normal'))
